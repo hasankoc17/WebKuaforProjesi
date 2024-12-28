44effaa01097851325a3f30e9fcc62c5c4a10501
hasankoc17 <133044867+hasankoc17@users.noreply.github.com>
2024-12-28 18:45:26 +0300

    servis ınterfaceinde guncelleme

--- OtoServisSatis.Service/Abstract/IService.cs
+++ OtoServisSatis.Service/Abstract/IService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using OtoServisSatis.Entities;
 using OtoServisSatis.Data;
 using OtoServisSatis.Data.Abstract;
-
+// ınterface ımplemantasyonu buradan yapılacak
 namespace OtoServisSatis.Service.Abstract
 {
     public interface IService<T> : IRepository<T> where T : class, IEntity, new()
